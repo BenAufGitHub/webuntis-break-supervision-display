a1638e1258d156cfaae67a833f76c47267757277
BenAufGitHub <benme04@gmail.com>
2022-11-29 11:21:31 +0100

    Bugfix: Multiples of periods removed + removed session-with-clause

--- src/UntisBreaks.py
+++ src/UntisBreaks.py
@@ -2,34 +2,32 @@ import datetime
 import webuntis
 
 
-def _group_by_start(break_supervisions: list) -> dict:
+def _group_by_start(break_superv: list) -> dict:
     result = {}
-    for bs in break_supervisions:
+    for bs in break_superv:
         if bs.start not in result.keys():
             result[bs.start] = []
         result[bs.start].append(bs)
     return result
 
 
-def _todays_supervisions(session: webuntis.Session) -> list:
-    all_supervisions = []
+def _todays_supervisions(session: webuntis.Session) -> set:
+    all_supervisions = set()
     today = datetime.datetime.today()
     current_day = datetime.datetime(today.year,today.month,today.day)
-
     for teacher in session.teachers():
-        periods = session.timetable(teacher=teacher.id, start=current_day, end=current_day)
-        teachers_supervisions = filter(lambda p: p.type == 'bs', periods)
-        all_supervisions += teachers_supervisions
+        break_superv = session.timetable(teacher=teacher.id, start=current_day, end=current_day).filter(type='bs')
+        for bs in break_superv:
+            all_supervisions.add(bs)
     return all_supervisions
 
 
 '''
-@param session: containing server, username, password, school and useragent information (do not login with the session)
+@param session: containing server, username, password, school and useragent information
 @return dict-keys: datetime (always from current day) / dict-values: list of all break-supervisions (webuntis.objects.PeriodObject)
 '''
 def get_todays_supervisions(session: webuntis.Session) -> dict:
-    with session.login() as s:
-        supervisions = _todays_supervisions(s)
-        return _group_by_start(supervisions)
+    supervisions = _todays_supervisions(session)
+    return _group_by_start(supervisions)
 
              
