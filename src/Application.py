0e3d0711df57c04cb54ea79904dafee313f01fbd
BenAufGitHub <benme04@gmail.com>
2022-11-29 22:04:53 +0100

    Setup for subframes (Login + Display)

--- src/Application.py
+++ src/Application.py
@@ -1,16 +1,23 @@
-from tkinter import *
-from tkinter.ttk import *
+import tkinter as tk
+from tkinter import ttk
 import webuntis
 
 
-class MainFrame(Tk):
+class Constants:
+    WIDTH=1000
+    HEIGHT=600
+    RESIZABLE=True
+    BACKGROUND="#93B5E1"
+
+
+class MainFrame(tk.Tk):
 
     # ================ public ================
 
 
     def __init__(self):
         super().__init__()
-        self.create_layout()
+        self.configure_variables()
         self.session = None
 
 
@@ -22,9 +29,10 @@ class MainFrame(Tk):
             self._logout()
 
 
-    def _before_start(self, session: webuntis.Session):
+    def _before_start(self, session: webuntis.Session) -> None:
         if self._prelogin(session):
-            self.selectDisplayPanel()
+            return self.selectDisplayFrame(session)
+        self.selectLoginPanel()
 
 
     # ============ session handling ===============
@@ -48,29 +56,45 @@ class MainFrame(Tk):
     # ========== front-end elements ================
 
 
-
-    def create_layout(self):
-        self.configure_variables()
-        # TODO
-
-
     def configure_variables(self):
+        self.resizable(Constants.RESIZABLE, Constants.RESIZABLE)
         screen_width = self.winfo_screenwidth()
         screen_height = self.winfo_screenheight()
-        width = 1000
-        height = 600
-        x = (screen_width-width)/2
-        y = (screen_height-height)/2
+        x = (screen_width-Constants.WIDTH)/2
+        y = (screen_height-Constants.HEIGHT)/2
 
         self.title('Pausenaufsicht')
-        self.geometry(f'{width}x{height}+{int(x)}+{int(y)}')
-        self.resizable(True, True)
+        self.geometry(f'{Constants.WIDTH}x{Constants.HEIGHT}+{int(x)}+{int(y)}')
         # TODO icon
 
 
-    def selectDisplayPanel(self):
-        pass
+    def selectDisplayFrame(self, session: webuntis.Session):
+        frame = DisplayFrame(parent=self)
+        frame.grid(row=0, column=0) 
+        frame.pack(anchor=tk.N, fill=tk.BOTH, expand=True, side=tk.LEFT )
+
 
     
-    def selectLoginPanel(self):
-        pass
+    def selectLoginFrame(self):
+        frame = LoginFrame(parent=self)
+        frame.grid(row=0, column=0)
+        frame.pack(anchor=tk.N, fill=tk.BOTH, expand=True, side=tk.LEFT )
+
+
+
+# ================== Frames =========================
+
+
+class FillerFrame(tk.Frame):
+    def __init__(self, parent):
+        super().__init__(parent, bg=Constants.BACKGROUND)
+
+
+
+class DisplayFrame(FillerFrame):
+    pass
+
+
+
+class LoginFrame(FillerFrame):
+    pass
