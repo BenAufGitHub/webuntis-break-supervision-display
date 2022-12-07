d798a9892048e794c3509dd13c78814208383c85
BenAufGitHub <benme04@gmail.com>
2022-12-07 23:48:43 +0100

    possible to change date of 'today' (for testing)

--- src/DisplayFrame.py
+++ src/DisplayFrame.py
@@ -17,6 +17,8 @@ class DisplayFrame(TKUtils.FillerFrame):
         self.is_today = True
         self.data = None
         self.currentBreak = None
+        # For pretending to have a different day, otherwise should be 0.
+        self.natural_offset = 0
         self._build()
 
 
@@ -42,24 +44,29 @@ class DisplayFrame(TKUtils.FillerFrame):
 
 
     # TODO try-catch connection -> send back to login frame + Error-Popup
+    '''
+    For the "current" day's table (or whatever the natural_offset suggests).
+    '''
     def fetch_break_info(self):
         def do():
-            self.data = UntisBreaks.get_todays_supervisions(self.session)
-            self.currentBreak = UntisBreaks.next_break_time(self.data.keys())
+            self.data = UntisBreaks.get_offset_supervisions(self.session, self.natural_offset)
+            current_time = datetime.now()+timedelta(days=self.natural_offset)
+            self.currentBreak = UntisBreaks.next_break_time(self.data.keys(), current=current_time)
         self._api_fail_save(do)
 
     
     def fetch_nextday_info(self):
         def do():
         # for Mo-Do => Move 1, else move to Monday
-            next_day = self._get_next_day()
+            next_day = self._get_next_day() + self.natural_offset
             self.data = UntisBreaks.get_offset_supervisions(self.session, next_day)
             self.currentBreak = UntisBreaks.next_break_time(self.data.keys())
         self._api_fail_save(do)
 
 
     def _get_next_day(self):
-        return 1 if datetime.now().weekday() < 4 else 7-datetime.now().weekday()
+        weekday = (datetime.now()+timedelta(days=self.natural_offset)).weekday()
+        return 1 if weekday < 4 else 7-weekday
 
 
     # ======== building =======>
@@ -151,10 +158,20 @@ class DisplayFrame(TKUtils.FillerFrame):
     '''
     def update_day_label(self):
         if hasattr(self, 'day_label') and self.day_label: self.day_label.destroy()
-        cb = self.currentBreak if self.currentBreak else datetime.now() if self.is_today else datetime.now()+timedelta(days=self._get_next_day())
-        self.day_label = TKUtils.DayLabel(self.settings_bar, cb, borderwidth=0)
+        ref_time = self._get_reference_day()
+        self.day_label = TKUtils.DayLabel(self.settings_bar, ref_time, borderwidth=0)
         self.day_label.config(font=("Arial", 13), bg=Constants.BACKGROUND)
         self.day_label.place(relx=0.95, rely=0.55, anchor=tk.E)
+
+
+    '''
+    A datetime object from the day of the shown table
+    '''
+    def _get_reference_day(self) -> datetime:
+        if self.currentBreak: return self.currentBreak
+        if self.is_today:
+            return datetime.now() + timedelta(days=self.natural_offset)
+        return datetime.now() + timedelta(days=self.natural_offset+self._get_next_day())
 
 
     # ======= exit bar =======>
