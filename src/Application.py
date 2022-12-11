b5d74603b226b82793de2097090a5029cc21f3fc
BenAufGitHub <benme04@gmail.com>
2022-12-11 17:17:44 +0100

    basic outline of Login Frame

--- src/Application.py
+++ src/Application.py
@@ -91,4 +91,82 @@ class MainFrame(tk.Tk):
 
 
 class LoginFrame(TKUtils.FillerFrame):
-    pass
+
+    def __init__(self, parent, *args, **kwargs):
+        super().__init__(parent, *args, **kwargs)
+        self.adjust_win_size(parent)
+        self._build()
+
+
+    def adjust_win_size(self, root):
+        pass # TODO
+
+
+    def _build(self):
+        self.login_panel = self._create_login_panel()
+        self.login_panel.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
+
+
+    def _create_login_panel(self):
+        panel = TKUtils.FillerFrame(self, width=230, height=350, highlightthickness=1, highlightbackground="white")
+        panel.grid_propagate(False)
+
+
+        panel.columnconfigure(0, weight=1)
+        for i, subframe in enumerate([*self._create_login_elements(panel)]):
+            panel.rowconfigure(i, weight=1)
+            subframe.grid(row=i, column=0, sticky=tk.W)
+        return panel
+
+    
+    def _create_login_elements(self, parent):
+        user = self._createUserEntry(parent)
+        password = self._createPasswordEntry(parent)
+        school = self._createSchoolEntry(parent)
+        server = self._createServerEntry(parent)
+        return user, password, school, server
+
+    
+    def _createUserEntry(self, parent):
+        user = TKUtils.FillerFrame(parent)
+        label = tk.Label(user, text="Benutzer:", pady=4, padx=4, bg=Constants.BACKGROUND, font=("Arial", 11))
+        self.user_entry = tk.Entry(user, width=25, font=("Arial", 11), borderwidth=6, relief=tk.FLAT)
+
+        self._entry_grid_configure(user, label, self.user_entry)
+        return user
+
+
+    def _createPasswordEntry(self, parent):
+        password = TKUtils.FillerFrame(parent)
+        label = tk.Label(password, text="Password:", pady=4, padx=4, bg=Constants.BACKGROUND, font=("Arial", 11))
+        self.pw_entry = tk.Entry(password, width=25, font=("Arial", 11), borderwidth=6, relief=tk.FLAT)
+
+        self._entry_grid_configure(password, label, self.pw_entry)
+        return password
+
+
+    def _createSchoolEntry(self, parent):
+        school = TKUtils.FillerFrame(parent)
+        label = tk.Label(school, text="Schule:", pady=4, padx=4, bg=Constants.BACKGROUND, font=("Arial", 11))
+        self.school_entry = tk.Entry(school, width=25, font=("Arial", 11), borderwidth=6, relief=tk.FLAT)
+
+        self._entry_grid_configure(school, label, self.school_entry)
+        return school
+
+
+    def _createServerEntry(self, parent):
+        server = TKUtils.FillerFrame(parent)
+        label = tk.Label(server, text="Server:", pady=4, padx=4, bg=Constants.BACKGROUND, font=("Arial", 11))
+        self.server_entry = tk.Entry(server, width=25, font=("Arial", 11), borderwidth=6, relief=tk.FLAT)
+
+        self._entry_grid_configure(server, label, self.server_entry)
+        return server
+
+    
+    def _entry_grid_configure(self, grid_parent, label, entry):
+        grid_parent.columnconfigure(0, weight=1)
+        grid_parent.rowconfigure(0, weight=1)
+        grid_parent.rowconfigure(1, weight=1)
+
+        label.grid(row=0, column=0, sticky=tk.W)
+        entry.grid(row=1, column=0, sticky=tk.NSEW, padx=4)
