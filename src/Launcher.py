d4ba6dbe8571f98f96bbc5d7a3a82d7e28b3db49
BenAufGitHub <benme04@gmail.com>
2022-12-11 23:03:03 +0100

    Error Handling (Launching)

--- src/Launcher.py
+++ src/Launcher.py
@@ -1,5 +1,6 @@
 import webuntis
 import src.Application as App
+import src.TKUtils as tku
 
 
 def launch(session: webuntis.Session=None):
@@ -7,5 +8,4 @@ def launch(session: webuntis.Session=None):
         root = App.MainFrame()
         root.mainloop(session=session)
     except Exception as e:
-        # TODO ModalWindow (something went wrong/see details/send info) => close
-        raise e
+        tku.TKErrorHandler.report_callback_exception(None, e.__class__.__name__, str(e), e.__traceback__)
